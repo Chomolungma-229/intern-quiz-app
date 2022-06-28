3b156b8007e1c02b93c1150b8d8ad64ae7796246
chomoranma229 <kokkororon871@gmail.com>
2022-06-28 18:16:57 +0900

    AngularとStrapiとPostgreSQLの構築

---
+++ strapi-app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a64c077fdd605ab8491be83f5ec7b667'),
+    },
+  },
+});
---
+++ strapi-app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b97591a-2091-4c36-a550-4c1a4ea9804f'
+};
