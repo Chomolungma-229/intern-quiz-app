3ab68397d6a1648b8ebe488755bce8f57d2eba72
chomoranma229 <kokkororon871@gmail.com>
2022-08-09 01:12:01 +0900

    言語選択の画面の作成 cssの修正

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { SelectLanguageComponent } from './pages/select-language/select-language.component';
 
 const routes: Routes = [
-  {path: 'quiz', component: QuizComponent},
-  {path: 'statistics', component: StatisticsComponent}
+  { path: 'quiz', component: QuizComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'selectlanguege', component: SelectLanguageComponent },
 ];
 
 @NgModule({
