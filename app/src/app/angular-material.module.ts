cc9401a6f04b036cf87b49eb80e192ef21b54094
chomoranma229 <kokkororon871@gmail.com>
2022-08-09 17:44:33 +0900

    footerの高さの固定

--- app/src/app/angular-material.module.ts
+++ app/src/app/angular-material.module.ts
@@ -7,7 +7,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
