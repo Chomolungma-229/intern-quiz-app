8d7e8cb77107fb505f2911d8127c8e8c7cb57ce7
chomoranma229 <kokkororon871@gmail.com>
2022-08-11 09:37:31 +0900

    ユーザー登録画面のモックアップ作成

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { SelectLanguageComponent } from './pages/select-language/select-language.component';
+import { RegisterUserComponent } from './pages/register-user/register-user.component';
 
 const routes: Routes = [
   { path: 'quiz', component: QuizComponent },
   { path: 'statistics', component: StatisticsComponent },
   { path: 'selectlanguege', component: SelectLanguageComponent },
+  { path: 'register-user', component: RegisterUserComponent },
+
 ];
 
 @NgModule({
--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  hiddenPath: string[] = ['login', 'signup'];
+  hidden: boolean = false;
+  constructor(
+    public router: Router,
+  ) { }
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(f => f instanceof NavigationEnd)
+    )
+      .subscribe((s: any) => {
+        this.hidden = this.hiddenPath.some(e => s.url.includes(e));
+      });
+  }
+
 }
