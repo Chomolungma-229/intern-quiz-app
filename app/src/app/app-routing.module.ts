f0a442850897cd34d029ee5c00a1103ff709670b
chomoranma229 <kokkororon871@gmail.com>
2022-08-17 19:16:15 +0900

    ホーム画面のモックアップ作成

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { QuizComponent } from './pages/quiz/quiz.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { SelectLanguageComponent } from './pages/select-language/select-language.component';
 import { RegisterUserComponent } from './pages/register-user/register-user.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'quiz', component: QuizComponent },
   { path: 'statistics', component: StatisticsComponent },
   { path: 'selectlanguege', component: SelectLanguageComponent },
   { path: 'register-user', component: RegisterUserComponent },
-
+  { path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
