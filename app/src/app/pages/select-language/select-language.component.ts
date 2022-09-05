47c4a7b60c80eeddc538f2477c694b6b74c598a3
chomoranma229 <kokkororon871@gmail.com>
2022-09-05 14:51:36 +0900

    問題の出力結果の表示

--- app/src/app/pages/select-language/select-language.component.ts
+++ app/src/app/pages/select-language/select-language.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { Language } from 'src/app/model/language';
 import { QuizService } from 'src/app/quiz.service';
 
 
@@ -10,14 +12,32 @@ import { QuizService } from 'src/app/quiz.service';
 })
 export class SelectLanguageComponent implements OnInit {
 
-  constructor(private quizservice:QuizService) { }
+  languages:Language[] = [];
+
+  constructor(private quizservice:QuizService, private router: Router) { }
 
   ngOnInit(): void {
     const query = {
     }
     this.quizservice.getSelectLanguage(query).subscribe(languages => console.log(languages));
+    this.quizservice.getSelectLanguage(query).subscribe(
+      ((language: Language[]) => {
+        this.languages = language;
+        })
+      );
 
+  }
 
+  toQuiz(languageId: number){
+    this.router.navigate(
+      ['/quiz'],
+      {
+        queryParams:
+        {
+          id: languageId
+        }
+      }
+    );
   }
 
 }
