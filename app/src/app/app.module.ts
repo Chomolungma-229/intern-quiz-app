7b1b8009faad46eadc5c3fb11ecf5473ee451fd0
chomoranma229 <kokkororon871@gmail.com>
2022-10-11 09:49:47 +0900

    Chart.jsのインポート

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { RegisterUserComponent } from './pages/register-user/register-user.component';
@@ -38,6 +39,7 @@ import { DialogComponent } from '../app/components/dialog/dialog.component';
     AngularMaterialModule,
     ReactiveFormsModule,
     HttpClientModule,
+    NgChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
