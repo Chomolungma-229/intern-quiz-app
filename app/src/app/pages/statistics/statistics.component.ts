0701475ccd33b4872a2f81961078b701741816b5
chomoranma229 <kokkororon871@gmail.com>
2022-10-11 10:29:03 +0900

    グラフの表示

--- app/src/app/pages/statistics/statistics.component.ts
+++ app/src/app/pages/statistics/statistics.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
+import { startWith } from 'rxjs';
 
 @Component({
   selector: 'app-statistics',
@@ -6,6 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      '9/30',
+      '10/1',
+      '10/2',
+      '10/3',
+      '10/4',
+      '10/5',
+
+    ],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        fill: false,
+        tension: 0,
+        borderColor: 'rgba(255,255,0,1)',
+        pointBackgroundColor: 'blue',
+      },
+      {
+        data: [0, 25, 50, 60, 40, 80, 90],
+        label: 'Series A',
+        fill: false,
+        tension: 0,
+        borderColor: 'blue',
+        pointBackgroundColor: 'blue',
+      },
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    scales: {
+      y: {
+        suggestedMin: 0,
+        suggestedMax: 100
+      }
+    },
+    responsive: false,
+  };
+  public lineChartLegend = true;
 
   constructor() { }
 
