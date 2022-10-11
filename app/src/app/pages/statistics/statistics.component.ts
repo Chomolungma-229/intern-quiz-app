3e7da23e0799dfc84b642c2fee5f959edfd4ad80
chomoranma229 <kokkororon871@gmail.com>
2022-10-11 17:40:01 +0900

    CSSの変更

--- app/src/app/pages/statistics/statistics.component.ts
+++ app/src/app/pages/statistics/statistics.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
-import { startWith } from 'rxjs';
+import 'dayjs/locale/ja';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-statistics',
@@ -9,31 +10,35 @@ import { startWith } from 'rxjs';
 })
 export class StatisticsComponent implements OnInit {
 
+  today: any = dayjs();
+
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: [
-      '9/30',
-      '10/1',
-      '10/2',
-      '10/3',
-      '10/4',
-      '10/5',
-
+      this.today.subtract(6, 'day').format("MM月DD日"),
+      this.today.subtract(5, 'day').format("MM月DD日"),
+      this.today.subtract(4, 'day').format("MM月DD日"),
+      this.today.subtract(3, 'day').format("MM月DD日"),
+      this.today.subtract(2, 'day').format("MM月DD日"),
+      this.today.subtract(1, 'day').format("MM月DD日"),
+      this.today.format("MM月DD日")
     ],
     datasets: [
       {
         data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
+        label: 'JavaScript',
         fill: false,
         tension: 0,
-        borderColor: 'rgba(255,255,0,1)',
-        pointBackgroundColor: 'blue',
+        borderColor: 'yellow',
+        backgroundColor: 'white',
+        pointBackgroundColor: 'yellow',
       },
       {
         data: [0, 25, 50, 60, 40, 80, 90],
-        label: 'Series A',
+        label: 'Java',
         fill: false,
         tension: 0,
         borderColor: 'blue',
+        backgroundColor: 'white',
         pointBackgroundColor: 'blue',
       },
     ]
